65e3db36ca6e6aeb707d572e5585ff40756a2f48
geovgy <54918343+geovgy@users.noreply.github.com>
2025-07-19 20:01:26 -0400

    Add and format # of suppliers in table

--- src/components/tables/status.tsx
+++ src/components/tables/status.tsx
@@ -2,16 +2,27 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from ".
 import { useRelayTest } from "@/hooks/useRelayTest";
 import { useSuppliers } from "@/hooks/useSuppliers";
 import { Loader2 } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export function StatusTable() {
   const refreshInterval = 5 * 60;
   const [countdown, setCountdown] = useState(refreshInterval);
 
   const { data: relayTestData, refetch, isLoading: isLoadingRelayTest, isFetching: isFetchingRelayTest } = useRelayTest();
-  // const { data: supplierData } = useSuppliers();
+  const { data: supplierData } = useSuppliers({
+    paginationLimit: 1000,
+    paginationCountTotal: true,
+  });
 
-  // console.log(supplierData?.supplier);
+  const data = useMemo(() => {
+    return relayTestData?.map((item) => {
+      const supplier = supplierData?.supplier.reduce((acc, supplier) => acc + supplier.services.reduce((count, service) => count + (service.service_id === item.chain ? 1 : 0), 0), 0);
+      return {
+        ...item,
+        supplier,
+      };
+    });
+  }, [relayTestData, supplierData]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -42,15 +53,17 @@ export function StatusTable() {
         <TableHeader>
           <TableRow>
             <TableHead>Chain</TableHead>
+            <TableHead># of Suppliers</TableHead>
             <TableHead>Status</TableHead>
             <TableHead>Latency</TableHead>
             <TableHead>Block</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {relayTestData && relayTestData.length > 0 ? relayTestData.map((item) => (
+          {data && data.length > 0 ? data.map((item) => (
             <TableRow key={item.chain}>
               <TableCell className="text-blue-600 font-medium">{item.chain}</TableCell>
+              <TableCell>{item.supplier ?? "--"}</TableCell>
               <TableCell className={item.status === "success" ? "text-green-500" : item.status === "error" ? "text-red-500" : "text-yellow-500"}>{item.status === "success" ? "✅ Healthy" : item.status === "error" ? "❌ Need attention" : "⏳ Loading"}</TableCell>
               <TableCell>{item.latency ?? "--"}ms</TableCell>
               <TableCell>{item.blockNumber ?? "--"}</TableCell>
